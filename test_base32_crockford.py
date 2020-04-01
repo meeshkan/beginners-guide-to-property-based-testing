263bcec8f42220d5ac01d73c76fb677aff3d01c4
Carolyn Stransky <carolynstransky@gmail.com>
2020-04-01 16:52:03 +0200

    Add explanatory comments to tests

--- test_base32_crockford.py
+++ test_base32_crockford.py
@@ -3,6 +3,8 @@ import hypothesis.strategies as some
 from hypothesis import given
 
 
+# This decoding scheme only works for non-negative integers
+# `min_value=0` restricts Hypothesis to only generate integers with a minimum value of zero
 @given(some.integers(min_value=0))
 def test_base32_crockford(input_int):
     assert base32_crockford.decode(base32_crockford.encode(input_int)) == input_int
--- test_gmpy_popcount.py
+++ test_gmpy_popcount.py
@@ -1,18 +1,22 @@
 import hypothesis.strategies as some
 import pytest
-from hypothesis import given
+from hypothesis import given, settings
 
 try:
     import gmpy2
 except ModuleNotFoundError:
     pass
 
-
+# Slower solution that converts the integer to a binary string
+# and then counts the occurences of the string "1" inside:
 def count_bits_slow(input_int):
     return bin(input_int).count("1")
 
 
 @pytest.mark.skip(reason="Avoid forcing to install gmpy2")
 @given(some.integers(min_value=0))
+# Sets the number of test cases to 500 rather than the default 100:
+@settings(max_examples=500)
+# Optimized solution using pygmp2 to count set bits in an integer:
 def test_gmpy2_popcount(input_int):
     assert count_bits_slow(input_int) == gmpy2.popcount(input_int)
